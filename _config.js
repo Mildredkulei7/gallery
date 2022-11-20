25b85443b3f6d888f71cfa820aab93dda4c26a71
Mildred Jepkosgei <83188018+Mildredkulei7@users.noreply.github.com>
2022-11-21 01:00:03 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -3,9 +3,9 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
 <<<<<<< HEAD
-    production: 'mongodb+srv://<mildred.kulei@student.moringaschool.com>:<kossy123$>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<mildred.kulei@student.moringaschool.com>:<kossy123$>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<mildred.kulei@student.moringaschool.com>:<kossy123$>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<mildredkulei>:<kossy123$>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<mildredkulei>:<kossy123$>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<mildredkulei>:<kossy123$>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 =======
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
