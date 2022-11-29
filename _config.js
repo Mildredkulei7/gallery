e43a6269f01033dd9dddd586516120f37200f806
Mildred Jepkosgei <83188018+Mildredkulei7@users.noreply.github.com>
2022-11-29 16:26:15 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -3,9 +3,9 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
 <<<<<<< HEAD
-    production: 'mongodb+srv://<mildredkulei>:<XkuAHvu3qkmQ3cVw>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<mildredkulei>:<XkuAHvu3qkmQ3cVw>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<mildredkulei>:<XkuAHvu3qkmQ3cVw>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<mildredkulei>:<Ik3ZYm45ntI9nAme>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<mildredkulei>:<Ik3ZYm45ntI9nAme>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<mildredkulei>:<Ik3ZYm45ntI9nAme>@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
 =======
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
